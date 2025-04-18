b56a05bd4cdf10142673ddc6799386b0b08fee54
Saw-Baw-Mu-Thaw <thawthibaw@gmail.com>
2025-04-18 16:47:13 +0700

    bug squashing
    the delete bug is fixed, found save bug also fixed, the login warning has also been fixed

--- www/index.php
+++ www/index.php
@@ -3,26 +3,30 @@ session_start();
 
 require 'skeletondb.php';
 
+$res = true;
+$error = "";
+
 // checks if user is logged in
 if (!isset($_SESSION['username'])) {
     header('Location: login.php');
 }
 
-$res = true;
-$error = "";
 $rows = get_notes($_SESSION['username']);
-
 
 
 // the code that handles deleting
 if (isset($_POST['delete']) && !empty($_POST['delete'])) {
 
     //send delete signal to db
-    $res = delete_note($_SESSION['username'], $_POST['delete']);
+    $dbres = delete_note($_SESSION['username'], $_POST['delete']);
     //then unlink file here
-    unlink("/notes/" . $_POST['delete']);
+    $fileres = unlink("notes/" . $_POST['delete'] . '.txt');
     //then reload this page
-    header('Refresh:0');
+    if (!$dbres || !$fileres) {
+        echo ('There\'s been a problem');
+    } else {
+        header('Refresh:0');
+    }
 }
 
 // code that handles the create redirect
